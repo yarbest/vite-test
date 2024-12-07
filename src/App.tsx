ae74147b1227aca250977ddc552d38c783386c1f
yarbest <yarbest15@gmail.com>
2024-12-07 11:54:08 +0200

    recommending settings for eslint from README

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.scss';
 
+// import { a } from './a.ts';
+
 function App() {
   const [count, setCount] = useState(0);
-
+  // a();
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
