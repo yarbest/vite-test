30da840bcbb3c8ce3511bf9ede878cba7e3f1a02
yarbest <yarbest15@gmail.com>
2024-12-07 14:32:45 +0200

    setup aliases in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@containers': path.resolve(__dirname, 'src/containers'),
+    },
+  },
   // build: {
   //   outDir: 'public',
   // },
