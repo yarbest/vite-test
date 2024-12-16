d42c93207a9710d65efe1bf816aabb2be3baa0d5
yarbest <yarbest15@gmail.com>
2024-12-16 13:31:10 +0200

    show only existing todo in the separate page

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,7 +1,24 @@
+import { selectListItems } from '@pages/TodosPage/store/selectors'
+import { Navigate, useParams } from 'react-router-dom'
+import { paths } from 'src/routes/paths'
+import { useAppSelector } from 'src/store'
+
+interface TodoPageParams extends Record<string, string | undefined> {
+  id: string
+}
+
 const TodoPage = () => {
+  const listItems = useAppSelector(selectListItems)
+  const { id } = useParams<TodoPageParams>()
+  const isIdInListItems = listItems.some(listItem => listItem.id === id)
+
+  if (!isIdInListItems) return <Navigate to={paths.notFound} replace />
+
   return (
     <div>
-      todo page
+      todo page:
+      {' '}
+      {id}
     </div>
   )
 }
