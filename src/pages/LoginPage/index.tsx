29d51152ca8e4bdf55b110a0dbe65534c11fb1e8
yarbest <yarbest15@gmail.com>
2024-12-16 12:29:13 +0200

    handle empty location state

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -12,7 +12,7 @@ const LoginPage = () => {
   const handleLogin = () => {
     login()
     // in location.state there is a location, which was requested before login, it is set in PrivateRoute
-    navigate((location.state as Location).pathname ?? paths.todos, { replace: true })
+    navigate((location.state as Location | undefined)?.pathname ?? paths.todos, { replace: true })
   }
   return (
     <Button onClick={handleLogin} label="Login" />
